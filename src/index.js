6c362008ec957a63694b596d3f1babe1e98b8fbf
galina <gpischalo@belta.by>
2020-09-29 23:21:20 +0300

    min, max, avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(!array || array.length===0){
+    return 0;
+   }
+   return array.reduce((prev,curr)=>{ return prev>=curr ? curr : prev});
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(!array || array.length===0){
+    return 0;
+   }
+   return array.reduce((prev,curr)=>{ return prev<=curr ? curr : prev});
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(!array || array.length===0){
+    return 0;
+   }
+   const sum = array.reduce((prev,curr)=> prev+curr );
+   return sum/array.length;
 }
